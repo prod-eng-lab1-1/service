ff7eb3a8e2098dad8e0b5a12c7cbd7d97e9d697f
Patrascu02 <147556504+Patrascu02@users.noreply.github.com>
2026-04-01 01:29:48 +0300

    alex-done-lab5

--- src/test/java/ro/unibuc/prodeng/e2e/CucumberSpringConfiguration.java
+++ src/test/java/ro/unibuc/prodeng/e2e/CucumberSpringConfiguration.java
@@ -6,6 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @CucumberContextConfiguration
 @SpringBootTest(classes = CucumberSpringConfiguration.class)
 public class CucumberSpringConfiguration {
-    // Empty configuration - we don't need Spring context for E2E tests
-    // since we're calling an already running server via REST
 }
